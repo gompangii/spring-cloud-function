5c0c9c32c53dfe0867d06a895bb0ff5807efd0a2
Gompangii <gompangii@gmail.com>
2025-01-20 16:52:32 +0900

    uppercase 추가

--- src/main/java/com/springcloud/springcloudfunction/SpringCloudFunctionApplication.java
+++ src/main/java/com/springcloud/springcloudfunction/SpringCloudFunctionApplication.java
@@ -2,6 +2,9 @@ package com.springcloud.springcloudfunction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
 
 @SpringBootApplication
 public class SpringCloudFunctionApplication {
@@ -10,4 +13,8 @@ public class SpringCloudFunctionApplication {
 		SpringApplication.run(SpringCloudFunctionApplication.class, args);
 	}
 
+	@Bean
+	public Function<String, String> uppercase() {
+		return value -> value.toUpperCase();
+	}
 }
