6a4908576565f17d01c48288755e629ef564185c
Gompangii <gompangii@gmail.com>
2025-01-20 17:29:07 +0900

    MyFunctionTest 에 @Componet 추가

--- src/main/java/com/springcloud/springcloudfunction/MyFunctionTest.java
+++ src/main/java/com/springcloud/springcloudfunction/MyFunctionTest.java
@@ -2,10 +2,12 @@ package com.springcloud.springcloudfunction;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
 @Configuration
+@Component
 public class MyFunctionTest {
 
   public Function<String, String> hello() {
